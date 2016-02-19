8391f443161c1a19f73db96226ad5b1f0bfd8a17
Imesh Chandrasiri <dimal.chandrasiri@gmail.com>
2016-02-19 14:47:12 +0530

    Responsive fix and bug fixes

--- markup/js/left-navigation.js
+++ markup/js/left-navigation.js
@@ -11,18 +11,21 @@ $(window).load(function(){
         }
 
         var el = $(this),
+            color = $(this).find('.left-menu-item').css('background-color'),
             prev = $(".navigation > a:first"),
             distance = el.offset().top  - $(".navigation").offset().top,
             isLastElClicked = el.next().length > 0 ? false : true;
 
         if (el.prev().length > 0) {
             animating = true;
+            $(el).find('.left-menu-item').css('background-color',color)
             $.when(
                 el.animate({
                     top: -distance
                 }, 700)
             ).done(function () {
                     el.insertBefore(prev).css('top','0px').addClass('active');
+                    el.find('.left-menu-item').removeAttr('style');
                     if(isLastElClicked){
                         prev.removeClass('active');
                     }else{
